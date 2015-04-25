d87098fe993d3a7c23c2f79fe58ed788c7f01b73
Adam Mesha <adam@mesha.org>
2015-04-25 22:28:20 +0300

    python version

---
+++ quine.py
@@ -0,0 +1,12 @@
+s = '''triple = "'" * 3
+def q(s):
+	return triple + s + triple
+
+print('s = ' + q(s))
+print(s)'''
+triple = "'" * 3
+def q(s):
+	return triple + s + triple
+
+print('s = ' + q(s))
+print(s)

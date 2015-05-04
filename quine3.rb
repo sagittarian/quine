7b036feba069f554cc04e2eeb730574b701ff76a
Adam Mesha <adam@mesha.org>
2015-05-04 23:37:00 +0300

    unwieldy ruby expression

---
+++ quine3.rb
@@ -0,0 +1,1 @@
+(->(op, rep){ (->(s){ s.method(op).call s.method(rep).call }) }).call(:concat, :inspect).call "(->(op, rep){ (->(s){ s.method(op).call s.method(rep).call }) }).call(:concat, :inspect).call "

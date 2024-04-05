1c40730f825ead43796cb86de326f90a7a28fb64
Adam Raizen <araizen@gmail.com>
2024-04-05 17:05:55 +0300

    Add a python quine with base64 encoding

---
+++ py/quine-b64.py
@@ -0,0 +1,2 @@
+import base64; (lambda b: print(f'{base64.b64decode(b).decode()}(\n"{b}")'))(
+"aW1wb3J0IGJhc2U2NDsgKGxhbWJkYSBiOiBwcmludChmJ3tiYXNlNjQuYjY0ZGVjb2RlKGIpLmRlY29kZSgpfShcbiJ7Yn0iKScpKQ==")
